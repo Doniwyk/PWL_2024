b22af2d7bb43ae62b44d137dfcc0e6cc73dcc847
doniwyk <doniwyk@gmail.com>
2024-02-28 15:39:39 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/about', function () {
+    return 'Doni Wahyu Kurniawan <br> 2241720015';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+Route::get('/article/{id}', function ($id) { 
+    return 'Artikel dengan id- '.$id; 
+});
+
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
 });
